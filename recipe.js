c4a9083ca6d4a219e50365df7f874707193ae149
Siyun Feng <siyunf21@gmail.com>
2024-11-30 02:31:13 -0500

    set up basic layout, added 3 recipes

---
+++ recipe.js
@@ -0,0 +1,34 @@
+const cuisines = ["Chinese", "American", "Mexican", "Japanese", "Korean", "Thai", "Vietnamese", "Bengali"];
+
+const courses = ["Appetizer", "Main", "Dessert", "Beverage"];
+
+const creamyMushroomSoup = {
+    dishName: "Creamy Mushroom Soup",
+    courses: "Appetizer",
+    serves: "4 to 6",
+    prepTime: 20,
+    totalTime: 50,
+    ingredients: [
+        "4 tablespoons (½ stick) unsalted butter (plus 4 to 6 pats serving)",
+        "1 small onion (finely chopped)",
+        "6 cloves garlic (minced)",
+        "3 lbs assorted mushrooms (trimmed and finely chopped)",
+        "1 tablespoon chopped fresh thyme or ½ tablespoon dried (plus more for garnish)",
+        "kosher salt and freshly ground black pepper",
+        "2 cups low-sodium chicken broth (plus more if necessary)",
+    ],
+    note: [
+        "Use a big soup pot"
+    ],
+    instructions: [
+        "Heat the butter in the pot over medium-high heat",
+        "Add the onion and stir about 8 minutes until softened",
+        "Add the garlic and cook 1 more minute",
+        "Add the mushrooms, thyme, 2 tablespoons salt, and ¼ teaspoon pepper, stir about 10 minutes until the mushrooms release water and shrink to half size",
+        "Add the chicken broth and bring to a boil, then reduce the heat and simmer 15 to 20 minutes until the mushrooms are super tender and the soup thickens slightly",
+        "Puree half of the soup (about 3 cups) in a blender or food processor until smooth, then stir it back into the pot",
+        "Thin with additional broth if needed, and season to taste with salt and pepper",
+        "Put a pat of butter and some thyme"
+    ],
+    recipeBy: "Chrissy Teigen"
+}
